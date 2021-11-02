9b57a47f0fab975919fb7e2759569d6902566e39
Владислав Сотников <stnkvld@gmail.com>
2021-11-02 07:42:35 +0300

    Добавляет компонент превью-карточки отеля

---
+++ src/components/HotelCard.vue
@@ -0,0 +1,77 @@
+<template>
+    <router-link
+        class="hotel-card"
+        :to="{ name: 'hotel', params: { id: info.id } }"
+    >
+        <article class="hotel-card__article">
+            <!--
+                В идеале Picture нужновынести в отдельный компонент,
+                но в данном случае оставил внутри данного, чтобы
+                сэкономить время
+            -->
+            <picture class="hotel-card__picture">
+                <source
+                    srcset="@/assets/images/hotel.webp, @/assets/images/hotel@2x.webp 2x"
+                    type="image/webp"
+                />
+                <img
+                    class="hotel-card__image"
+                    src="@/assets/images/hotel.jpg"
+                    srcset="@/assets/images/hotel@2x.jpg 2x"
+                    alt="Небольшой отель с бассейном"
+                    width="300"
+                    height="180"
+                    loading="lazy"
+                />
+            </picture>
+            <h3 class="hotel-card__title">
+                {{ info.title }}
+            </h3>
+            <p class="hotel-card__description">
+                {{ info.body }}
+            </p>
+        </article>
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: 'HotelCard',
+    props: {
+        info: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .hotel-card {
+        display: block;
+        color: $color;
+        text-decoration: none;
+
+        &__image {
+            display: block;
+            width: 300px;
+            height: 180px;
+            object-fit: cover;
+            margin-bottom: 10px;
+            border-radius: 6px;
+        }
+
+        &__title {
+            font-size: 20px;
+            line-height: 1.35;
+            margin: 0;
+            margin-bottom: 10px;
+        }
+
+        &__description {
+            font-size: 12px;
+            line-height: 1.55;
+            margin: 0;
+        }
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="home">
+    <div class="container">
+      <HotelCard :info="{ title: 'Test Title', body: 'Test Description' }" />
+    </div>
+  </section>
+</template>
+
+<script>
+import HotelCard from '@/components/HotelCard.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HotelCard
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
